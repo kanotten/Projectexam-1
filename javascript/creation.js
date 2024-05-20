9287e2a650247589af3cbb0df2698d7fd02e55f2
jakob <jakobhkrohn@gmail.com>
2024-05-20 21:12:39 +0200

    use session tokens

--- javascript/creation.js
+++ javascript/creation.js
@@ -1,59 +1,29 @@
-var accessToken = ""
-var apiKey = ""
+async function getAllPosts() {
+  const apiKey = localStorage.getItem("apiKey");
+  const accessToken = localStorage.getItem("accessToken");
 
-async function do_log_in() {
-  const email = "jk123@stud.noroff.no"
-  const password = "12345678"
   try {
-    const response = await fetch("https://v2.api.noroff.dev/auth/login", {
-      method: "POST",
+    const response = await fetch("https://v2.api.noroff.dev/social/posts", {
+      method: "GET",
       headers: {
         "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, password }),
-    });
-
-    if (response.ok) {
-      const responseData = await response.json();
-      console.log(responseData);
-      accessToken = responseData.data.accessToken;
-      console.log("accessToken: " + accessToken);
-    }
-
-    return response.ok;
-  } catch (error) {
-    console.error("Error:", error);
-  }
-  return "";
-}
-
-async function get_api_key() {
-  try {
-    const response = await fetch("https://v2.api.noroff.dev/auth/create-api-key", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "accept": "application/json",
         Authorization: `Bearer ${accessToken}`,
+        "X-Noroff-API-Key": apiKey
       },
-      body: JSON.stringify({ name: "My API Key" }),
     });
 
     if (response.ok) {
       const responseData = await response.json();
-      console.log(responseData);
-      apiKey = responseData.data.key;
-      let status = responseData.data.status;
-      console.log("apiKey: " + apiKey);
-      console.log("status: " + status);
+      console.log(responseData)
     }
 
-    return response.ok;
   } catch (error) {
-    console.error("Error:", error);
+    console.error("failed to get all posts:", error);
+    return
   }
-  return "";
 }
+
+getAllPosts()
 
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("blogPostForm");
@@ -64,14 +34,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const postTitle = document.getElementById("postTitle").value;
     const postContent = document.getElementById("postContent").value;
 
+    const apiKey = localStorage.getItem("apiKey");
+    const accessToken = localStorage.getItem("accessToken");
+
     const postData = {
       title: postTitle,
       content: postContent,
     };
 
     try {
-      await do_log_in()
-      await get_api_key()
       const response = await fetch("https://v2.api.noroff.dev/social/posts", {
         method: "POST",
         headers: {
